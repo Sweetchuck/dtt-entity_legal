75deee88aeb6c0ab2525e18751fd034b9e78215a
Andor <david-andor@kozpontiagy.hu>
2022-07-31 14:03:57 +0200

    Add entity_legal_document_acceptance:create Behat step

--- src/Behat/Context/EntityLegal.php
+++ src/Behat/Context/EntityLegal.php
@@ -7,7 +7,11 @@ namespace Sweetchuck\DrupalTestTraits\EntityLegal\Behat\Context;
 use Behat\Behat\Hook\Scope\AfterScenarioScope;
 use Behat\Behat\Hook\Scope\BeforeScenarioScope;
 use Behat\Behat\Hook\Scope\ScenarioScope;
+use Behat\Gherkin\Node\TableNode;
+use Drupal\entity_legal\EntityLegalDocumentInterface;
+use PHPUnit\Framework\Assert;
 use Sweetchuck\DrupalTestTraits\Core\Behat\Context\Base;
+use Sweetchuck\DrupalTestTraits\Core\Behat\Context\Entity as EntityContext;
 
 class EntityLegal extends Base {
 
@@ -83,6 +87,92 @@ class EntityLegal extends Base {
       $this->acceptManuallyScenarioTag,
       $scope->getScenario()->getTags(),
     );
+  }
+
+  /**
+   * @Given :documentLabel entity legal acceptances:
+   *
+   * @code
+   * # Minimal.
+   * Given "my_terms" entity legal acceptances:
+   *   | uid   |
+   *   | Admin |
+   * @endcode
+   *
+   * @code
+   * # Long.
+   * Given "my_terms" entity legal acceptances:
+   *   | document_version_name | uid   | acceptance_date |
+   *   | my_terms_12345        | Admin | yesterday       |
+   * @endcode
+   */
+  public function doEntityLegalAcceptanceCreate(string $documentLabel, TableNode $table) {
+    $entityContext = $this->getContext(EntityContext::class);
+
+    /** @var \Drupal\entity_legal\EntityLegalDocumentInterface $document */
+    $document = $entityContext->getEntityByLabel(
+      'entity_legal_document',
+      $documentLabel,
+    );
+    Assert::assertNotNull(
+      $document,
+      sprintf(
+      'There is no Entity Legal Document with label: "%s"',
+        $documentLabel,
+      ),
+    );
+
+    $acceptancesFieldValues = $this->convertTableRowsToDocumentAcceptancesFieldValuesList(
+      $document,
+      $table,
+      $this->getDocumentAcceptanceDefaultValues($document),
+    );
+
+    foreach ($acceptancesFieldValues as $acceptanceFieldValues) {
+      $entityContext->createContentEntity('entity_legal_document_acceptance', $acceptanceFieldValues);
+    }
+  }
+
+  public function convertTableRowsToDocumentAcceptancesFieldValuesList(
+    EntityLegalDocumentInterface $document,
+    TableNode $table,
+    array $default
+  ): array {
+    $fieldValuesList = [];
+    foreach ($table->getColumnsHash() as $row) {
+      $fieldValuesList[] = $this->convertTableRowToDocumentAcceptanceFieldValues($document, $row, $default);
+    }
+
+    return $fieldValuesList;
+  }
+
+  public function convertTableRowToDocumentAcceptanceFieldValues(
+    EntityLegalDocumentInterface $document,
+    array $row,
+    array $default
+  ): array {
+    if (empty($row['document_version_name']) && !empty($default['document_version_name'])) {
+      $row['document_version_name'] = $default['document_version_name'];
+    }
+
+    $row['acceptance_date'] = !empty($row['acceptance_date']) ?
+      strtotime($row['acceptance_date'])
+      : ($default['acceptance_date'] ?? NULL);
+
+    return $row;
+  }
+
+  public function getDocumentAcceptanceDefaultValues(EntityLegalDocumentInterface $document): array {
+    $default = [
+      'acceptance_date' => time(),
+    ];
+
+    $documentVersion = $document->getPublishedVersion();
+    if ($documentVersion) {
+      $default['document_version_name'] = $documentVersion->label();
+    }
+
+    return $default;
   }
 
 }
